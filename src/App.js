34e6e42848ba0e05ca21449d09243863d7983638
joamessina <joaquin.messina@gmail.com>
2022-02-23 01:12:55 -0300

    emprolijando codigo

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
 
   return (
     <div className="App">
-      <h1> App</h1>
+      <h1>My Login/Logout App Tutorial</h1>
       {
         isAuthenticated ? <LogoutButton/> : <LoginButton/>
       }
